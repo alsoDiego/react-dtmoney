6505f6299cc7224d0ab0b765af5b9cf35619dff5
Diego Soares <as.diego@live.com>
2021-04-04 16:11:43 -0300

    finish api comunicating

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,9 +3,9 @@ import Modal from 'react-modal';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg';
+import { api } from '../../services/api';
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
-import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
